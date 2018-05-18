fcc16fa01228283587b9c085ae7c3bb6e2f299fb
thejaswinidm <thejaswini360@gmail.com>
2018-05-18 06:36:14 -0700

    assignment2 and 3

--- Week-2/thejaswini/1.py
+++
@@ -1,5 +0,0 @@
-list = input('enter the elements in the list').split()
-n=len(list)
-for i in range(0,n,2):
-	del list[i]
-print(list)
--- Week-2/thejaswini/2.py
+++
@@ -1,4 +0,0 @@
-list=input('enter the elements of list').split()
-n=len(list)
-list.sort(key=len)
-print(list)
